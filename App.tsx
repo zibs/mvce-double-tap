679eb8094960fed394553bc12e29c4fca048c016
Eli Zibin <elizibin@gmail.com>
2022-01-26 14:08:42 -0800

    add single, double, triple tap example

--- App.tsx
+++ App.tsx
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
+import Animated from "react-native-reanimated";
 
 export default function App() {
+  const singleTap = Gesture.Tap()
+    .numberOfTaps(1)
+    .maxDuration(250)
+    .onEnd((_event, success) => {
+      if (success) {
+        console.log("single tap!");
+      }
+    });
+  const doubleTap = Gesture.Tap()
+    .numberOfTaps(2)
+    .maxDuration(250)
+    .onEnd((_event, success) => {
+      if (success) {
+        console.log("double tap!");
+      }
+    });
+  const tripleTap = Gesture.Tap()
+    .numberOfTaps(3)
+    .maxDuration(250)
+    .onEnd((_event, success) => {
+      if (success) {
+        console.log("trip tap!");
+      }
+    });
+
+  const taps = Gesture.Exclusive(tripleTap, doubleTap, singleTap);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={styles.container}>
+      <GestureDetector gesture={taps}>
+        <Animated.View
+          style={{ height: 50, width: 50, backgroundColor: "purple" }}
+        ></Animated.View>
+      </GestureDetector>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
